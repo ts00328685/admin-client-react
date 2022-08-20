deb4cefc845686ed79a93890c0c9f1dd557c63d8
Ryan Tseng <ryan.tseng@webcomm.com.tw>
2022-08-20 23:26:03 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import Dashboard from './page/dashboard/Dashboard';
+import SignIn from './page/login/SignIn';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+// function useRoutes(arg0: { path: string; element: JSX.Element; }[]) {
+//   throw new Error('Function not implemented.');
+// }
+
